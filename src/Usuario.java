d921d9a543e253e11d0e52c04d9b114877e760a4
Breno <brenoholandax@gmail.com>
2023-01-02 23:23:26 -0300

    my first commit

---
+++ src/Usuario.java
@@ -0,0 +1,21 @@
+public class Usuario 
+{
+    public static void main(String[] args)  throws Exception
+ {
+    SmartTv smartTv = new SmartTv();
+
+System.out.printtLn("Tv Ligada ?  " + smartTv.ligada );
+System.out.printtLn("Canala Atual :  " + smartTv.canal );
+System.out.printtLn("Volume atual? : " + smartTv.volume );
+
+smartTv.ligar  ();
+System.out.printtLn(" Novo Status - > Tv Ligada ?  " + smartTv.ligada );
+
+
+
+
+
+        
+ }
+    
+}
